8caa4f3f82e4a2d7718370352115517ad897d111
Colby M. White <colbywhite@users.noreply.github.com>
2024-10-25 10:11:57 -0500

    feat: rm auth, pnpm

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,11 @@
 import { Link } from 'react-router-dom'
 import { ThemeSwitcher } from '#/components/theme'
-import { Alert, Button } from '#/components/ui-react-aria'
-import { useAuthentication } from '#/context/auth/AuthProvider'
 
 // Assets in public directory cannot be imported from JavaScript.
 // Instead, we use `src/assets` directory.
 import ViteLogo from '../assets/images/vite.svg'
 
 export default function Home() {
-  const { user, loggedIn } = useAuthentication()
-
   return (
     <div className="mx-auto flex h-full min-h-screen w-full flex-col">
       <header className="mb-auto w-full p-4" aria-hidden>
@@ -25,37 +21,13 @@ export default function Home() {
             Vite + React + Typescript + Tailwind CSS + React Hook Form + Vitest
           </p>
         </div>
-        <div className="mt-4 flex flex-col items-center justify-center gap-2 sm:flex-row sm:gap-3">
-          <Link to="/dashboard" className="inline-flex items-center justify-center">
-            <Button variant="primary">User Dashboard</Button>
-          </Link>
-          <Link to="/admin" className="inline-flex items-center justify-center">
-            <Button variant="destructive">Admin Dashboard</Button>
-          </Link>
-          <Link
-            to="https://github.com/riipandi/vite-react-template"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Button variant="secondary" className="inline-flex items-center justify-center">
-              Get Source Code
-            </Button>
-          </Link>
-        </div>
-        <div>
-          <Alert variant={loggedIn ? 'info' : 'warning'} className="w-full text-center">
-            {loggedIn ? `Welcome back ${user?.email} 👋` : 'You are not logged in!'}
-          </Alert>
-        </div>
       </div>
       <footer className="mt-auto py-5 text-center">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <p className="text-gray-600 text-sm tracking-wide dark:text-gray-400">
-            &copy; {new Date().getFullYear()} - Made by{' '}
-            <Link to="https://ripandis.com" className="hover:underline">
-              Aris Ripandi
-            </Link>{' '}
-            in 🇮🇩
+            <Link to="https://github.com/riipandi/vite-react-template" className="hover:underline">
+              riipandi/vite-react-template
+            </Link>
           </p>
           <p className="mt-2 text-gray-600 text-sm tracking-wide dark:text-gray-400">
             v{import.meta.env.APP_VERSION}
